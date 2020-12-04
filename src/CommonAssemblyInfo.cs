ec94c76929a78c750393a02ad3f7f1b2ce230e39
Homayoun <behzadian@gmail.com>
2020-12-04 16:24:30 +0330

    NH updated

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -2,7 +2,7 @@ using System.Reflection;
 
 [assembly: AssemblyProduct("FluentNHibernate")]
 [assembly: AssemblyCompany("http://fluentnhibernate.org")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: AssemblyVersion("1.1.0.533")]
+[assembly: AssemblyFileVersion("1.1.0.533")]
+[assembly: AssemblyInformationalVersion("1.1.0.533")]
 [assembly: AssemblyCopyright("Copyright 2008-2018 James Gregory and contributors (Paul Batum, Hudson Akridge, Gleb Chermennov, Jorge Rodríguez Galán). All rights reserved.")]
